644bd168347be73672d1b73726606621ee216065
Ana Claudia <anacnogueira@gmail.com>
2026-04-17 16:40:27 -0300

    Criando componente de table

--- resources/views/email-list/index.blade.php
+++ resources/views/email-list/index.blade.php
@@ -5,14 +5,25 @@
         </x-h2>
     </x-slot>
     <x-card>
-        @forelse ($emailLists as $list)
-            Mostrar listas
-        @empty
+        @unless ($emailLists->isEmpty())
+            <x-table :headers="['#', __('Email List'), __('# Subscribers'), __('Actions')]">
+                <x-slot name="body">
+                    @foreach ($emailLists as $list)
+                        <tr>
+                            <x-table.td>{{ $list->id }}</x-table.td>
+                            <x-table.td>{{ $list->title }}</x-table.td>
+                            <x-table.td>{{ $list->subscribers()->count() }}</x-table.td>
+                            <x-table.td></x-table.td>
+                        </tr>
+                    @endforeach
+                </x-slot>
+            </x-table>
+        @else
             <div class="flex justify-center">
                 <x-link-button :href="route('email-list.create')">
                     {{ 'Register my first list' }}
                 </x-link-button>
             </div>
-        @endforelse
+        @endunless
     </x-card>
 </x-app-layout>
